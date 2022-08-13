068768244587f2c5190b5d58d1b6523a46f754fc
GLADYS <mungaiglado106@gmail.com>
2022-08-13 13:49:46 +0300

    intial commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
+
+
+
+
+
+
+
+#puts "Hello world!"
+
+#print "Hello world!"
+
+#p [1, 2, 3]
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }]
+
+#puts adds a line break at the end of a string
+
+# puts method returns nil
+
